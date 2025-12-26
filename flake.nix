d44065c424ba198e731e68d25878f6b19940b90e
haruki7049 <tontonkirikiri@gmail.com>
2025-12-26 14:28:10 +0900

    Create flakes

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-compat.url = "github:edolstra/flake-compat";
+    nix-gleam.url = "github:arnarg/nix-gleam";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          app = pkgs.buildGleamApplication {
+            src = lib.cleanSource ./.;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.nix-gleam.overlays.default
+            ];
+          };
+
+          treefmt = {
+            projectRootFile = ".git/config";
+
+            # Nix
+            programs.nixfmt.enable = true;
+
+            # Gleam
+            programs.gleam.enable = true;
+
+            # GitHub Actions
+            programs.actionlint.enable = true;
+
+            # Markdown
+            programs.mdformat.enable = true;
+          };
+
+          packages = {
+            inherit app;
+            default = app;
+          };
+
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              # Nix LSP
+              pkgs.nil
+
+              # Gleam-lang
+              pkgs.gleam
+              pkgs.erlang
+              pkgs.nodePackages.nodejs
+              pkgs.deno
+              pkgs.bun
+            ];
+          };
+        };
+    };
+}
